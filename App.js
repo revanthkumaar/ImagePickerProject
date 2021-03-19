b09744a361b93f3f1e6ca12f5c9a111dd01c201f
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-19 14:17:38 +0530

    lakjsdl
    alkjda

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Fragment, Component} from 'react';
-import ImagePicker from 'react-native-image-picker'
+import ImagePicker from 'react-native-image-picker';
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,8 +10,9 @@ import {
   Image,
   Button,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
+
 import {
   Header,
   LearnMoreLinks,
@@ -20,63 +21,75 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-
 const options = {
-  title:'Select Avatar',
-  customButtons:[{name:'fb',title:'Choose Phto frm Facebook'}],
-  storageOptions:{
+  title: 'Select Avatar',
+  customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+  storageOptions: {
     skipBackup: true,
-    patch:'images'
+    path: 'images',
+  },
+};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filepath: {
+        data: '',
+        uri: '',
+      },
+      fileData: '',
+      fileUri: '',
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <Text
+              style={{textAlign: 'center', fontSize: 20, paddingBottom: 10}}>
+              Pick Images from Camera & Gallery
+            </Text>
+            <View style={styles.ImageSections}>
+              <View>
+                {this.renderFileData()}
+                <Text style={{textAlign: 'center'}}>Base 64 String</Text>
+              </View>
+              <View>
+                {this.renderFileUri()}
+                <Text style={{textAlign: 'center'}}>File Uri</Text>
+              </View>
+            </View>
+
+            <View style={styles.btnParentSection}>
+              <TouchableOpacity
+                onPress={this.chooseImage}
+                style={styles.btnSection}>
+                <Text style={styles.btnText}>Choose File</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={this.launchCamera}
+                style={styles.btnSection}>
+                <Text style={styles.btnText}>Directly Launch Camera</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={this.launchImageLibrary}
+                style={styles.btnSection}>
+                <Text style={styles.btnText}>
+                  Directly Launch Image Library
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+      </Fragment>
+    );
   }
 }
-
-
-export default class App extends Component {
-
-  
-
-
-}
-
-render() {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.body}>
-          <Text style={{textAlign:'center',fontSize:20,paddingBottom:10}} >Pick Images from Camera & Gallery</Text>
-          <View style={styles.ImageSections}>
-            <View>
-              {this.renderFileData()}
-              <Text  style={{textAlign:'center'}}>Base 64 String</Text>
-            </View>
-            <View>
-              {this.renderFileUri()}
-              <Text style={{textAlign:'center'}}>File Uri</Text>
-            </View>
-          </View>
-
-          <View style={styles.btnParentSection}>
-            <TouchableOpacity onPress={this.chooseImage} style={styles.btnSection}  >
-              <Text style={styles.btnText}>Choose File</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={this.launchCamera} style={styles.btnSection}  >
-              <Text style={styles.btnText}>Directly Launch Camera</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={this.launchImageLibrary} style={styles.btnSection}  >
-              <Text style={styles.btnText}>Directly Launch Image Library</Text>
-            </TouchableOpacity>
-          </View>
-
-        </View>
-      </SafeAreaView>
-    </Fragment>
-  );
-}
-};
-
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -89,25 +102,25 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     height: Dimensions.get('screen').height - 20,
-    width: Dimensions.get('screen').width
+    width: Dimensions.get('screen').width,
   },
   ImageSections: {
     display: 'flex',
     flexDirection: 'row',
     paddingHorizontal: 8,
     paddingVertical: 8,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   images: {
     width: 150,
     height: 150,
     borderColor: 'black',
     borderWidth: 1,
-    marginHorizontal: 3
+    marginHorizontal: 3,
   },
   btnParentSection: {
     alignItems: 'center',
-    marginTop:10
+    marginTop: 10,
   },
   btnSection: {
     width: 225,
@@ -116,12 +129,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 3,
-    marginBottom:10
+    marginBottom: 10,
   },
   btnText: {
     textAlign: 'center',
     color: 'gray',
     fontSize: 14,
-    fontWeight:'bold'
-  }
+    fontWeight: 'bold',
+  },
 });
